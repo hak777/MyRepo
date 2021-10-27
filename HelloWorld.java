09bc8bd763b208a678c93d0394221e62161284ca
Hakob <hak777@live.ru>
2021-10-27 08:49:47 +0000

    new read file program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld{
     public static void main(String args[]){
-     System.out.println("Hello fgvznxzfrom Java");
+     System.out.println("Hello fgvznxzfrom Java"
+);
+
     }
 }
 
